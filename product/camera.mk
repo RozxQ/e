805f356e6cad6ef945465f7f887a2a077a487264
Hyakei <oke123543@gmail.com>
2018-10-18 05:41:46 +0700

    revert fix gcam

--- product/camera.mk
+++ product/camera.mk
@@ -9,7 +9,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     camera.msm8937 \
     libmm-qcamera
-	
+
 # Camera HAL
 PRODUCT_PACKAGES += \
     camera.device@1.0-impl \
@@ -18,40 +18,18 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
-
+ 
 # Snap
 PRODUCT_PACKAGES += \
     Snap
-
+ 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
     camera.display.umax=1920x1080 \
     camera.display.lmax=1280x720 \
-    camera.hal1.packagelist=com.skype.raider,com.google.android.talk,org.telegram.messenger,com.whatsapp \
-    camera2.portability.force_api=1 \
+    camera.hal1.packagelist=com.skype.raider,com.google.android.talk \
     media.camera.ts.monotonic=1 \
     persist.camera.gyro.android=1 \
-    persist.camera.HAL3.enabled=1 \
-    persist.camera.is_type=2 \
-    persist.camera.gyro.disable=1 \
-    persist.camera.time.monotonic=1 \
-    persist.ts.postmakeup=true \
-    persist.ts.rtmakeup=true \
-    vidc.enc.narrow.searchrange=1 \
-    media.msm8956.version=0 \
-    media.msm8956hw=0 \
-    persist.media.treble_omx=false \
-    mm.enable.smoothstreaming=true \
-    mmp.enable.3g2=true \
-    media.aac_51_output_enabled=true \
-    media.stagefright.audio.sink=280 \
-    av.debug.disable.pers.cache=1 \
-    vidc.enc.narrow.searchrange=1 \
-    drm.service.enabled=true \
-    vidc.disable.split.mode=1 \
-    vendor.audio.hw.aac.encoder=true \
-    vendor.vidc.enc.disable_bframes=1 \
-    vendor.vidc.dec.downscalar_width=1920 \
-    vendor.vidc.dec.downscalar_height=1088 \
-    vendor.vidc.disable.split.mode=1 \
-    vendor.vidc.enc.disable.pq=true
+    persist.camera.HAL3.enabled=0 \
+    persist.camera.is_type=1 \
+    vidc.enc.narrow.searchrange=1
